5f121431f5a3c02a963b6b19692ac28779980a52
peyman malek <p.malek32@gmail.com>
2022-08-08 13:03:18 +0430

    make components folder structure and routes , sidebar , footer

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Navbar ,} from './navbar/Navbar'
+export {default as Cryptocurrencies ,} from './Cryptocurrencies/Cryptocurrencies'
+export {default as Homepage ,} from './homepage/Homepage'
+export {default as CryptoDetails ,} from './CryptoDetails/CryptoDetails'
+export {default as News ,} from './News/News'
+export {default as Exchanges ,} from './exchanges/Exchanges'
